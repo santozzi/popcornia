8766269ad1563215023e07d467e76e722a41b279
Sergio Antozzi <santozzi@gmail.com>
2023-12-22 18:17:13 -0300

    pre entrega

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,23 @@ import PropTypes from 'prop-types';
 
 const Footer = ({}) => {
 	return (
-		<div className='footer'>
- 			Footer works!
- 		</div>
+		<footer>
+		<div class="logo">S. J. A.</div>
+		<div class="socialMedias">
+		  <div>
+			<i class="fa-brands fa-github"></i
+			><a href="https://github.com/santozzi" target="_blank">Github</a>
+		  </div>
+		  <div>
+			<i class="fa-brands fa-linkedin"></i
+			><a
+			  href="https://www.linkedin.com/in/sergio-antozzi-961891173/"
+			  target="_blank"
+			  >Linkedin</a
+			>
+		  </div>
+		</div>
+	  </footer>
 	);
 };
 
